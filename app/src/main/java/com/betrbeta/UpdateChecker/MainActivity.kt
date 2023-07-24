9da52e8ffbdf527d1292346b31bba1ca01c47f8b
Yan Shin <irrfan15@gmail.com>
2023-07-24 16:33:30 +0630

    Add button for navigation

--- app/src/main/java/com/betrbeta/UpdateChecker/MainActivity.kt
+++ app/src/main/java/com/betrbeta/UpdateChecker/MainActivity.kt
@@ -9,12 +9,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,9 +59,9 @@ class MainActivity : ComponentActivity() {
 
 @RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun UpdateCheckUI() {
+private fun UpdateCheckUI() {
     val developerUpdatedOn = remember { mutableStateOf("") }
     val userUpdatedOn = remember { mutableStateOf("") }
 
@@ -64,37 +71,62 @@ fun UpdateCheckUI() {
         modifier = Modifier
             .fillMaxSize()
             .background(Color(rgb(240, 240, 240)))
-        ,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = if (isAppOutdated) "The installed app is out of date." else "The installed app is up to date.")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(rgb(240, 240, 240))),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.End
 
-        if (isAppOutdated) {
-            Text(text = "Days out of date: $daysOutOfDate")
+            ){
+                Icon(
+                    painter = painterResource(id = R.drawable.noti_icon),
+                    contentDescription = "Notification",
+                    modifier = Modifier.clickable(
+                        onClick = {
+                            //TODO: Add navigation to noti page
+                        }))
+            }
+
+            Spacer(modifier = Modifier.height(150.dp))
+
+            Text(
+                text = if (isAppOutdated) "The installed app is out of date."
+                else "The installed app is up to date.")
+
+            if (isAppOutdated) {
+                Text(text = "Days out of date: $daysOutOfDate")
+            }
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(4.dp),
+                value = developerUpdatedOn.value,
+                onValueChange = { developerUpdatedOn.value = it },
+                placeholder = { Text(text = stringResource(R.string.devUpdateString)) },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                visualTransformation = DateTransform(),
+                singleLine = true
+            )
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(4.dp),
+                value = userUpdatedOn.value,
+                onValueChange = { userUpdatedOn.value = it },
+                placeholder = { Text(text = stringResource(R.string.userUpdateString)) },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                visualTransformation = DateTransform(), //makes input visually have dashes
+                singleLine = true
+            )
         }
 
-        OutlinedTextField(
-            modifier = Modifier
-                .padding(4.dp),
-            value = developerUpdatedOn.value,
-            onValueChange = { developerUpdatedOn.value = it },
-            placeholder = { Text(text = stringResource(R.string.devUpdateString)) },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            visualTransformation = DateTransform(),
-            singleLine = true
-        )
-
-        OutlinedTextField(
-            modifier = Modifier
-                .padding(4.dp),
-            value = userUpdatedOn.value,
-            onValueChange = { userUpdatedOn.value = it },
-            placeholder = { Text(text = stringResource(R.string.userUpdateString)) },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            visualTransformation = DateTransform(), //makes input visually have dashes
-            singleLine = true
-        )
     }
 }
 
